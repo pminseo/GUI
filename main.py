dfda2f0830c8cc0d6dabd623701f467b9c67dc27
pminseo <se0m0q@gmail.com>
2022-09-27 17:08:00 +0900

    sub2Images add

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import math
 from SliderWidget import SliderWidget
 from CheckWidget import CheckWidget
 
+import io
+
 class ImageProcessing(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -27,12 +29,17 @@ class ImageProcessing(QMainWindow):
         self.binaryAct = QAction("Binarization")
         self.arithAct = QAction("Arithmetic Operation")
         self.edgeAct = QAction("Edge Detection")
+        self.histogramAct = QAction("Hostogram Equalization")
+        self.add2imgsAct = QAction("Addition of two images")
+        self.sub2imgsAct = QAction("Subtraction of two images")
         
         self.imgArea = QWidget()
         self.inImg = 0
+        self.inImg2 = 0
         self.outImg = 0
         
         self.inImgLabel = QLabel()
+        self.inImgLabel2 = QLabel()
         self.outImgLabel = QLabel()
         
         self.initUI()
@@ -46,6 +53,9 @@ class ImageProcessing(QMainWindow):
         self.imgMenu.addAction(self.binaryAct)
         self.imgMenu.addAction(self.arithAct)
         self.imgMenu.addAction(self.edgeAct)
+        self.imgMenu.addAction(self.histogramAct)
+        self.imgMenu.addAction(self.add2imgsAct)
+        self.imgMenu.addAction(self.sub2imgsAct)
         
         self.mBar.addMenu(self.fileMenu)
         self.mBar.addMenu(self.imgMenu)
@@ -57,9 +67,13 @@ class ImageProcessing(QMainWindow):
         self.binaryAct.triggered.connect(self.binarization)
         self.arithAct.triggered.connect(self.arithmetic)
         self.edgeAct.triggered.connect(self.edgeDetection)
+        self.histogramAct.triggered.connect(self.histogramEqual)
+        self.add2imgsAct.triggered.connect(self.add2Images)
+        self.sub2imgsAct.triggered.connect(self.sub2Images)
         
         hbox = QHBoxLayout()
         hbox.addWidget(self.inImgLabel)
+        hbox.addWidget(self.inImgLabel2)
         hbox.addWidget(self.outImgLabel)
         self.imgArea.setLayout(hbox)
         
@@ -68,12 +82,14 @@ class ImageProcessing(QMainWindow):
         self.setStatusBar(self.sBar)
         
         self.setFixedSize(540,300)
+        self.viewMode = 2
         
         self.show()
 
     def openFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', './', "BMP files (*.bmp);; JPG files(*.jpg);; All files(*.*)")
+        fname = self.loadFile()
         im = Image.open(fname[0])
+        if fname[0].split(".")[-1] in ['pgm', 'ppm', 'PGM', 'PPM']: im = im.resize((256,256))
         
         self.inImg = np.array(im)
         w = self.inImg.shape[1]
@@ -103,6 +119,10 @@ class ImageProcessing(QMainWindow):
     def binarization(self):
         w = self.inImg.shape[1]
         h = self.inImg.shape[0]
+            
+        if self.viewMode != 2:
+            self.viewMode = 2
+            self.setviewMode(w,h)
         
         self.outImg = self.inImg.copy()
         
@@ -119,6 +139,11 @@ class ImageProcessing(QMainWindow):
     def arithmetic(self):
         w = self.inImg.shape[1]
         h = self.inImg.shape[0]
+        
+        if self.viewMode != 2:
+            self.viewMode = 2
+            self.setviewMode(w,h)
+            
         qImg = QImage(self.inImg.data, w,h, QImage.Format_Grayscale8)
         self.outImgLabel.setPixmap(QPixmap.fromImage(qImg))
         
@@ -136,6 +161,11 @@ class ImageProcessing(QMainWindow):
     def edgeDetection(self):
         w = self.inImg.shape[1]
         h = self.inImg.shape[0]
+        
+        if self.viewMode != 2:
+            self.viewMode = 2
+            self.setviewMode(w,h)
+            
         qImg = QImage(self.inImg.data, w,h, QImage.Format_Grayscale8)
         self.outImgLabel.setPixmap(QPixmap.fromImage(qImg))
         
@@ -186,6 +216,108 @@ class ImageProcessing(QMainWindow):
                 
         qImg = QImage(self.outImg.data, w, h, QImage.Format_Grayscale8)
         self.outImgLabel.setPixmap(QPixmap.fromImage(qImg))
+        
+    def histogramEqual(self):
+        w = self.inImg.shape[1]
+        h = self.inImg.shape[0]
+            
+        if self.viewMode != 2:
+            self.viewMode = 2
+            self.setviewMode(w,h)
+        self.outImg = self.inImg.copy()
+        hist = [0 for i in range(256)]
+        acc_hist = 0
+        for j in range(h):
+            for i in range(w):
+                k = self.inImg[i][j]
+                hist[k] += 1
+        cumulativeSum = []
+        for i in range(256):
+            acc_hist += hist[i]
+            cumulativeSum.append(acc_hist)
+        for j in range(h):
+            for i in range(w):
+                k = self.inImg[i][j]
+                self.outImg[i][j] = cumulativeSum[k]/(256*256)*255
+                
+        qImg = QImage(self.outImg.data, w,h, QImage.Format_Grayscale8)
+        self.outImgLabel.setPixmap(QPixmap.fromImage(qImg))
+        
+    def setviewMode(self, w, h):
+        self.setFixedSize((w + 14) * self.viewMode, h + 44)
+        if self.viewMode == 2:
+            self.inImg2 = 0
+            self.inImgLabel2.setPixmap(QPixmap())
+    
+    def loadFile(self):
+        fname = QFileDialog.getOpenFileName(self, 'Select the First Image', './', "BMP files (*.bmp);; PGM files (*.pgm);; JPG files(*.jpg);; All files(*.*)")
+        return fname
+    
+    def add2Images(self):
+        self.viewMode = 3
+        fname = self.loadFile()
+        im = Image.open(fname[0])
+        if fname[0].split(".")[-1] in ['pgm', 'ppm', 'PGM', 'PPM']: im = im.resize((256,256))
+
+        self.inImg = np.array(im)
+        w = self.inImg.shape[1]
+        h = self.inImg.shape[0]
+        self.setviewMode(w,h)
+        qImg = QImage(self.inImg.data, w, h, QImage.Format_Grayscale8)
+        self.inImgLabel.setPixmap(QPixmap.fromImage(qImg))
+        
+        fname2 = self.loadFile()
+        im2 = Image.open(fname2[0])
+        if w != im2.size[0] or h != im2.size[1]:
+            im2 = im2.resize((w,h))
+        self.inImg2 = np.array(im2)
+        
+        qImg2 = QImage(self.inImg2.data, w, h, QImage.Format_Grayscale8)
+        self.inImgLabel2.setPixmap(QPixmap.fromImage(qImg2))
+        self.outImg = self.inImg.copy()
+        
+        alpha, beta = 1, 1
+        for j in range(h):
+            for i in range(w):
+                val = int((alpha * self.inImg[j][i]) + (beta * self.inImg2[j][i]))
+                self.outImg[j][i] = 255 if val > 255 else val
+                
+        qImg3 = QImage(self.outImg.data, w, h, QImage.Format_Grayscale8)
+        self.outImgLabel.setPixmap(QPixmap.fromImage(qImg3))
+        
+    def sub2Images(self):
+        self.viewMode = 3
+        fname = self.loadFile()
+        im = Image.open(fname[0])
+        if fname[0].split(".")[-1] in ['pgm', 'ppm', 'PGM', 'PPM']: im = im.resize((256,256))
+
+        self.inImg = np.array(im)
+        w = self.inImg.shape[1]
+        h = self.inImg.shape[0]
+        self.setviewMode(w,h)
+        qImg = QImage(self.inImg.data, w, h, QImage.Format_Grayscale8)
+        self.inImgLabel.setPixmap(QPixmap.fromImage(qImg))
+        
+        fname2 = self.loadFile()
+        im2 = Image.open(fname2[0])
+        if w != im2.size[0] or h != im2.size[1]:
+            im2 = im2.resize((w,h))
+        self.inImg2 = np.array(im2)
+        
+        qImg2 = QImage(self.inImg2.data, w, h, QImage.Format_Grayscale8)
+        self.inImgLabel2.setPixmap(QPixmap.fromImage(qImg2))
+        self.outImg = self.inImg.copy()
+        
+        print(self.inImg.shape, self.inImg2.shape)
+
+        alpha, beta = 1, 1
+        for j in range(h):
+            for i in range(w):
+                val = int((alpha * self.inImg[j][i]) - (beta * self.inImg2[j][i]))
+                self.outImg[j][i] = 0 if val < 0 else val
+                
+        qImg3 = QImage(self.outImg.data, w, h, QImage.Format_Grayscale8)
+        self.outImgLabel.setPixmap(QPixmap.fromImage(qImg3))
         
 if __name__ == '__main__':
     try:
